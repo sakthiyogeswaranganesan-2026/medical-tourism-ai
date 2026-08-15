48bffa8d23c0157daf2abc3144bfab6358bd8479
Sakthi Gaja Y <yogeswarb.ganesan_bec28@mepcoeng.ac.in>
2026-08-15 10:25:13 +0530

    Connect frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@
 // BACKEND URL
 // ======================================================
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://medical-tourism-ai.onrender.com";
 
 
 // ======================================================
